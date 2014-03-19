6d3f27f9a49857c5b560726bd9773ef5e56230e0
shuchun <shuchuneric@163.com>
2014-03-19 16:46:09 +0800

    add UUID Util file and test

---
+++ src/com/shuchun/test/UUIDTest.java
@@ -0,0 +1,20 @@
+package com.shuchun.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.shuchun.util.UUIDUtil;
+
+public class UUIDTest {
+
+	@Test(expected=IllegalArgumentException.class)
+	public void getUUIDTest() {
+		assertNotEquals(UUIDUtil.getUUID(),UUIDUtil.getUUID());
+		String[] ids=UUIDUtil.getUUID(2);
+		assertNotEquals(ids[0],ids[1]);
+		System.out.println(ids[0]);
+		UUIDUtil.getUUID(-1);
+	}
+}
--- src/com/shuchun/util/UUIDUtil.java
+++ src/com/shuchun/util/UUIDUtil.java
@@ -4,26 +4,32 @@ import java.util.*;
 
 public class UUIDUtil {
 	
-	static UUID id=UUID.randomUUID(); 
+	/**
+	 * 产生一个UUID
+	 * @return		UUID
+	 */
+	public static String getUUID(){
+		return UUID.randomUUID().toString().replace("-", "");
+	}
 	
-	public static void main(String[] args){
-		for(int i=0;i<10;i++){
-			System.out.println(UUID.randomUUID());
+	/**
+	 * 产生指定数量的UUID
+	 * @param number		UUID数量
+	 * @return				UUID数组
+	 */
+	public static String[] getUUID(int number){
+		
+		if(number<=0){
+			throw new IllegalArgumentException(UUIDUtil.class.getName()+"Exception: 数量小于0");
+		}
+
+		String[] ids=new String[number];
+		
+		for(int i=0;i<number;i++){
+			ids[i]=UUIDUtil.getUUID();
 		}
 		
-		/*System.out.println("Least:"+id.getLeastSignificantBits());
-		System.out.println("Most:"+id.getMostSignificantBits());
-		System.out.println(id.toString());
-		System.out.println("Variant:"+id.variant()+"|Version:"+id.version());
-		String[] seg=id.toString().split("-");
-		int bit=0;
-		
-		for(int i=0;i<seg.length;i++){
-			System.out.println("seg["+i+"]"+seg[i]+":"+seg[i].length());
-			bit+=(seg[i].length()*4);
-		}
-		
-		System.out.println("size:"+bit);*/
+		return ids;
 	}
 
 }
